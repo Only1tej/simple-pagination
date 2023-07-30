3122e3bd9152694d094312072336117f40522088
only1tej <tijanienny3@gmail.com>
2023-07-30 01:23:43 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Posts from "./components/Posts";
+import "./App.css";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+  // console.log(posts);
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading} />
+    </div>
+  );
+}
+
+export default App;
